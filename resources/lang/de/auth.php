2296f90e93263e846b93de326faaf4ac9aaf82ef
NKnuelle <Nils.Kretschmer@Mail.de>
2020-03-17 18:09:04 +0100

    Initial Commit

---
+++ resources/lang/de/auth.php
@@ -0,0 +1,19 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Authentication Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used during authentication for various
+    | messages that we need to display to the user. You are free to modify
+    | these language lines according to your application's requirements.
+    |
+    */
+
+    'failed' => 'Deine Eingaben konnten nicht gefunden werden.',
+    'throttle' => 'Zu viele Login-Versuche. Bitte probiere es in :seconds Sekunden erneut.',
+
+];
---
+++ resources/views/bottom_navigation.blade.php
@@ -0,0 +1,16 @@
+<ul class="nav fixed-bottom d-md-none pb-2 pl-3 pt-2 bg-dark">
+  <li class="nav-item">
+    <button type="button" class="btn btn-primary btn-lg ">
+      <span class="oi oi-list-rich"></span>
+      </button>
+  </li>
+  <li class="nav-item">
+    <button type="button" class="btn btn-secondary btn-lg ml-2">
+      <span class="oi oi-book"></span>
+      </button>
+  </li>
+</ul>
+
+
+
+
